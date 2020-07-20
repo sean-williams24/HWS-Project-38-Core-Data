af142ff00ef1504e17d5fa1b081c72809e3ad99d
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-07-20 11:35:55 +0100

    Initial Commit

---
+++ Project 38 - Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 38 - Core Data
+//
+//  Created by Sean Williams on 20/07/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
