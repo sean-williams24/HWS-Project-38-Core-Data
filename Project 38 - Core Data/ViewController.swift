60600d11c4cf25f1cca5e492eebfb59c43ee099a
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-07-22 13:11:16 +0100

    feat: instantiate persistent container, load persistent stores and write saveContext method

--- Project 38 - Core Data/ViewController.swift
+++ Project 38 - Core Data/ViewController.swift
@@ -6,13 +6,33 @@
 //  Copyright © 2020 Sean Williams. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var container: NSPersistentContainer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        container = NSPersistentContainer(name: "Project 38 - Core Data")
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+    
+    func saveConext() {
+        if container.viewContext.hasChanges {
+            do {
+                try container.viewContext.save()
+            } catch {
+                print("An error occured while saving: \(error)")
+            }
+        }
     }
 
 
