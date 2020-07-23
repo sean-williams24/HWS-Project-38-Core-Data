b2e31d44e979d76729e8d751393fb1ecfb8f7318
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-07-23 09:36:09 +0100

    feat: implement load saved data method creating fetch requests, sorting by date. Implement tableview delegates. Set merge policy on view context to object merge

--- Project 38 - Core Data/ViewController.swift
+++ Project 38 - Core Data/ViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 class ViewController: UITableViewController {
     
     var container: NSPersistentContainer!
+    var commits = [Commit]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,8 @@ class ViewController: UITableViewController {
         container = NSPersistentContainer(name: "Project 38 - Core Data")
         
         container.loadPersistentStores { storeDescription, error in
+            self.container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+            
             if let error = error {
                 print(error)
             }
@@ -26,6 +29,7 @@ class ViewController: UITableViewController {
         
         performSelector(inBackground: #selector(fetchCommits), with: nil)
         
+        loadSavedData()
     }
     
     @objc func fetchCommits() {
@@ -41,8 +45,9 @@ class ViewController: UITableViewController {
                     let commit = Commit(context: self.container.viewContext)
                     self.configure(commit: commit, usingJson: jsonCommit)
                 }
+                self.saveConext()
+                self.loadSavedData()
             }
-            saveConext()
         }
     }
     
@@ -64,8 +69,41 @@ class ViewController: UITableViewController {
             }
         }
     }
+    
+    func loadSavedData() {
+        let request = Commit.createFetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [sortDescriptor]
+        
+        do {
+            commits = try container.viewContext.fetch(request)
+            print("Fetched \(commits.count) commits")
+            tableView.reloadData()
+        } catch {
+            print("Error fetching commits")
+        }
+    }
 
 
+    //MARK: - TableView
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+
+        let commit = commits[indexPath.row]
+        cell.textLabel!.text = commit.message
+        cell.detailTextLabel!.text = commit.date.description
+
+        return cell
+    }
 }
 
 
